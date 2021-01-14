589d81aa80db50dab35897fa7cfa9c2c90ab7e0c
Alexandros Polichronopoulos <ap@pc14.eu>
2021-01-14 18:31:07 +0200

    building demo

---
+++ src/main/java/com/paymentcomponents/sepa/ParseInvalidPacs002.java
@@ -0,0 +1,138 @@
+package com.paymentcomponents.sepa;
+
+import gr.datamation.sepa.core.messages.eba.pacs.FIToFIPaymentStatusReport;
+
+public class ParseInvalidPacs002 {
+
+    public static void main(String... args) {
+        execute();
+    }
+
+    public static void execute() {
+        makeValidPacs002Invalid();
+        parseInvalidPacs002();
+    }
+
+    public static void makeValidPacs002Invalid() {
+        //You have to initiate the message object using the suitable constructor.
+        //In order to parse and validate a pacs.002 you need to use FIToFIPaymentStatusReport whose relations are available inside the object
+        //    rootElement = "FIToFIPmtStsRptS2",
+        //    namespace = "urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03S2",
+        //    xsd = "/xsd/eba/pacs.002.001.03S2.xsd"
+        FIToFIPaymentStatusReport messageObject = new FIToFIPaymentStatusReport();
+        try {
+            //Use parseAndValidateString() to fill the messageObject the content of the message and validate it
+            //validation fills the message variable errors object with any issue found during validation
+            messageObject.parseAndValidateString(validPacs002String);
+
+            //Manually edit a value of the message that makes it invalid
+            messageObject.getRootMessage().getGrpHdr().getInstgAgt().setFinInstnId(null);
+
+            //Use validate() to check the messageObject and validate the content
+            //message variable errors is filled with any issue found during validation
+            messageObject.getErrors().clear();
+            messageObject.validate();
+            Utils.printValidMessageOrErrors(messageObject);
+
+            /* The following error message should be visible when running this example:
+            Message is invalid, and the errors are the following:
+            Line: -1 -- cvc-complex-type.2.4.b: The content of element 'InstgAgt' is not complete. One of '{"urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03S2":FinInstnId}' is expected.
+             */
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Message cannot be parsed");
+        }
+    }
+
+    public static void parseInvalidPacs002() {
+        //You have to initiate the message object using the suitable constructor.
+        //In order to parse and validate a pacs.002 you need to use FIToFIPaymentStatusReport whose relations are available inside the object
+        //    rootElement = "FIToFIPmtStsRptS2",
+        //    namespace = "urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03S2",
+        //    xsd = "/xsd/eba/pacs.002.001.03S2.xsd"
+        FIToFIPaymentStatusReport messageObject = new FIToFIPaymentStatusReport();
+        try {
+            //Use parseAndValidateString() to fill the messageObject the content of the message and validate it
+            //validation fills the message variable errors object with any issue found during validation
+            messageObject.parseAndValidateString(invalidPacs002String);
+            Utils.printValidMessageOrErrors(messageObject);
+
+            /* The following error message should be visible when running this example:
+            Message is invalid, and the errors are the following:
+            Line: 8 -- cvc-complex-type.2.4.b: The content of element 'InstgAgt' is not complete. One of '{"urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03S2":FinInstnId}' is expected.
+             */
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Message cannot be parsed");
+        }
+    }
+
+    private static final String validPacs002String = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03S2\">\n" +
+            "    <FIToFIPmtStsRptS2>\n" +
+            "        <GrpHdr>\n" +
+            "            <MsgId>0006936999</MsgId>\n" +
+            "            <CreDtTm>2010-02-23T14:29:02.0Z</CreDtTm>\n" +
+            "            <InstgAgt>\n" +
+            "                <FinInstnId>\n" +
+            "                    <BIC>BCYPCY2N</BIC>\n" +
+            "                </FinInstnId>\n" +
+            "            </InstgAgt>\n" +
+            "        </GrpHdr>\n" +
+            "        <OrgnlGrpInfAndSts>\n" +
+            "            <OrgnlMsgId>BCYPCY2N20100223152604686001</OrgnlMsgId>\n" +
+            "            <OrgnlMsgNmId>pacs.004</OrgnlMsgNmId>\n" +
+            "            <OrgnlNbOfTxs>4</OrgnlNbOfTxs>\n" +
+            "            <OrgnlCtrlSum>1200</OrgnlCtrlSum>\n" +
+            "            <GrpSts>ACCP</GrpSts>\n" +
+            "            <StsRsnInf>\n" +
+            "                <Orgtr>\n" +
+            "                    <Id>\n" +
+            "                        <OrgId>\n" +
+            "                            <BICOrBEI>DEUTDEFF</BICOrBEI>\n" +
+            "                        </OrgId>\n" +
+            "                    </Id>\n" +
+            "                </Orgtr>\n" +
+            "                <Rsn>\n" +
+            "                    <Prtry>B00</Prtry>\n" +
+            "                </Rsn>\n" +
+            "            </StsRsnInf>\n" +
+            "        </OrgnlGrpInfAndSts>\n" +
+            "    </FIToFIPmtStsRptS2>\n" +
+            "</Document>";
+
+    private static final String invalidPacs002String = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pacs.002.001.03S2\">\n" +
+            "    <FIToFIPmtStsRptS2>\n" +
+            "        <GrpHdr>\n" +
+            "            <MsgId>0006936999</MsgId>\n" +
+            "            <CreDtTm>2010-02-23T14:29:02.0Z</CreDtTm>\n" +
+            "            <InstgAgt>\n" +
+//            "                <FinInstnId>\n" +
+//            "                    <BIC>BCYPCY2N</BIC>\n" +
+//            "                </FinInstnId>\n" +
+            "            </InstgAgt>\n" +
+            "        </GrpHdr>\n" +
+            "        <OrgnlGrpInfAndSts>\n" +
+            "            <OrgnlMsgId>BCYPCY2N20100223152604686001</OrgnlMsgId>\n" +
+            "            <OrgnlMsgNmId>pacs.004</OrgnlMsgNmId>\n" +
+            "            <OrgnlNbOfTxs>4</OrgnlNbOfTxs>\n" +
+            "            <OrgnlCtrlSum>1200</OrgnlCtrlSum>\n" +
+            "            <GrpSts>ACCP</GrpSts>\n" +
+            "            <StsRsnInf>\n" +
+            "                <Orgtr>\n" +
+            "                    <Id>\n" +
+            "                        <OrgId>\n" +
+            "                            <BICOrBEI>DEUTDEFF</BICOrBEI>\n" +
+            "                        </OrgId>\n" +
+            "                    </Id>\n" +
+            "                </Orgtr>\n" +
+            "                <Rsn>\n" +
+            "                    <Prtry>B00</Prtry>\n" +
+            "                </Rsn>\n" +
+            "            </StsRsnInf>\n" +
+            "        </OrgnlGrpInfAndSts>\n" +
+            "    </FIToFIPmtStsRptS2>\n" +
+            "</Document>";
+
+}
